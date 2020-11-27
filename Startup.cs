39f3713078d0557606bb796bcbc875ece1a5225a
frmauro <frmauro8@gmail.com>
2020-11-27 16:34:49 -0300

    implementation swagger api doc

--- Startup.cs
+++ Startup.cs
@@ -33,12 +33,24 @@ namespace SalesProductApi
                         .UseMySql(Configuration.GetConnectionString("myConnection"), mySqlOptions =>
                             mySqlOptions.EnableRetryOnFailure(3))
                             .EnableDetailedErrors()
-                        );       
+                        );  
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();                             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ProductContext context)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
