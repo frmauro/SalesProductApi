7c5ab868e8325abcc88328444c779e9c26697f55
frmauro <frmauro8@gmail.com>
2020-12-04 18:12:14 -0300

    change sgbd mysql to sqlserver

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using SalesProductApi.DB;
 
@@ -29,8 +22,7 @@ namespace SalesProductApi
         {
             services.AddCors();
             services.AddControllers();
-            //services.AddDbContext<ProductContext>(options => options
-              //          .UseSqlServer(Configuration.GetConnectionString("myConnection")));  
+            services.AddDbContext<ProductContext>(options => options.UseSqlServer(Configuration.GetConnectionString("myConnection")));  
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen();                             
         }
@@ -76,3 +68,4 @@ namespace SalesProductApi
         }
     }
 }
+
