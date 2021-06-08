f6d332f5e5c3caec62121ca14ef737233706cb52
frmauro <frmauro8@gmail.com>
2021-06-08 17:32:51 -0300

    implementation grpc service

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using SalesProductApi.DB;
 
 namespace SalesProductApi
@@ -66,6 +71,11 @@ namespace SalesProductApi
             {
                 endpoints.MapGrpcService<salesproductapi.Services.ProductService>();
                 endpoints.MapControllers();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
             });
         }
         
