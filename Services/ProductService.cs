bfc4fc62667a6dafae8a811d0e96406e36ae468e
frmauro <frmauro8@gmail.com>
2021-06-10 18:04:04 -0300

    change ProductService class

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -15,5 +15,24 @@ namespace salesproductapi.Services
                 Message = "Product - id: " + request.Id + " description: " + request.Description + " amount: " + request.Amount + " price: " + request.Price + " status: " + request.Status
             });
         }
+
+
+        public override Task<SalesProductApi.ItemResponse> GetProducts(SalesProductApi.Empty request, ServerCallContext context)
+        {
+                    List<SalesProductApi.ProductResponse> products = new List<SalesProductApi.ProductResponse>();
+                    SalesProductApi.ItemResponse response = new SalesProductApi.ItemResponse();
+                    SalesProductApi.ProductResponse product = new SalesProductApi.ProductResponse();
+
+                    product.Id = 1;
+                    product.Description = "Product 001";
+                    product.Amount = "200";
+                    product.Price = "200";
+                    product.Status = "Active";
+                    products.Add(product);
+                    
+                    response.Items.AddRange(products);
+
+                    return Task.FromResult(response);
+        }
     }
 }
